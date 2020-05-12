3900b4632221609363772c043ef57297b73e090c
freek <freek@spatie.be>
2020-05-12 16:52:11 +0200

    wip

--- config/videos.php
+++ config/videos.php
@@ -3,44 +3,31 @@
 return [
     'chapters' => [
         [
-            'title' => 'Using Mailcoach',
+            'title' => 'Creating a PHP package',
             'videos' => [
-                '380361695', // intro
-                '402762711', // using the one click installer
-                '380458377', // requiring confirmation
-                '381544501', // using form subscriptions
-                '380438228',  // adding a subscription form to your Laravel app
-                '381197983', // Preventing invalid subscriptions using a honeypot
-                '380445658', // validating a subscription
-                '383483633', // using segments
-                '380472359', // configuring welcome mails
-                '383416335', // Using events to add extra logic
+                '417225584', // intro
+                '417225712', // starting a PHP package using the skeleton
+                '417226543', // configuring the namespace and adding code
+                '417226860', // running tests on GitHub actions
+                '417227634', // enforcing a code style
+                '417227966', // publishing a package
+                '417228265', // creating a new release
             ]
         ],
         [
-            'title' => 'How Mailcoach was built',
+            'title' => 'Creating a Laravel Package',
             'videos' => [
-                '381834001', // Sending a campaign
-                '383386187', // Tracking opens and clicks
-                '383499729', // Building small interface elements using Blade X
-                '380520777', // Building filters, part 1: determining the right queries
-                '380697568', // Building filters, part 2: rendering the html using Blade X
-                '382696329', // rate limiting jobs
-                '382665546', // high level feature tests
-                '381539561', // using pending objects
-                '384774191', // conditionally rendering form elements
+
             ]
         ],
         [
-            'title' => 'Writing clear code',
+            'title' => 'Source diving Spatie packages',
             'videos' => [
-                '381650670', // refactor complex conditionals,
-                '381665810', // refactor if blocks to dedicated classes
-                '382652846', // controlling the flow of time
-                '384796029', // snapshot testing
-                '381569201', // using form requests
-                '381550441', // use suffixes,
-                '381691672', // create dedicated exception classes
+                '417225584', // Mailcoach,
+                '417225584', // Flash
+                '417225584', // Backup
+                '417225584', // response cache
+                '417225584', // tail
             ]
         ]
     ]

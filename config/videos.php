f48b743a066f20733f3668e3109aa17e75c54eee
freek <freek@spatie.be>
2020-05-12 19:45:25 +0200

    wip

--- config/videos.php
+++ config/videos.php
@@ -17,17 +17,26 @@ return [
         [
             'title' => 'Creating a Laravel Package',
             'videos' => [
-
+                // starting a Laravel package using the skeleton
+                // exploring the service provider
+                // running tests locally using Orchestra
+                // running tests on GitHub actions (+ supporting multiple laravel versions)
+                // reading config
+                // using migrations
+                // using views
+                // adding routes
+                // adding a facade
+                // creating a simple package
             ]
         ],
         [
             'title' => 'Source diving Spatie packages',
             'videos' => [
-                '417225584', // Mailcoach,
+                '417225584', // Tail
                 '417225584', // Flash
                 '417225584', // Backup
-                '417225584', // response cache
-                '417225584', // tail
+                '417225584', // Response cache
+                '417225584', // Mailcoach,
             ]
         ]
     ]

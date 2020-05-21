b01af629ae9e099ea061fe09d0f6a5ffcb196dbf
freek <freek@spatie.be>
2020-05-22 01:37:40 +0200

    wip

--- resources/views/front/home/partials/instructor.blade.php
+++ resources/views/front/home/partials/instructor.blade.php
@@ -1,18 +1,21 @@
-
 <div class="grid items-center gap-16 lg:cols-2">
 
-    <blockquote class="border-l-4 border-blue-100 overflow-hidden max-w-3xl bg-gray-100 shadow-xl p-8 | md:p-12 md:py-16 | xl:-ml-32 xl:px-16 xl:-mb-32">
+    <blockquote
+        class="border-l-4 border-blue-100 overflow-hidden max-w-3xl bg-gray-100 shadow-xl p-8 | md:p-12 md:py-16 | xl:-ml-32 xl:px-16 xl:-mb-32">
         <div class="text-xl leading-loose font-medium text-blue-400">
             <h3 class="markup-h3 text-lg opacity-50">Why create packages?</h3>
 
             <p class="mt-8">
-                By moving common functionalities to packages, the codebase of your apps can become smaller and more maintainable.
+                By moving common functionalities to packages, the codebase of your apps can become smaller and more
+                maintainable.
             </p>
             <p class="mt-4">
-                Packages can have their own testsuite. When a bug is discovered, you can easily distribute the fix in your apps by merely running <code class="opacity-75 text-lg bg-white rounded p-1">composer update</code>.
+                Packages can have their own testsuite. When a bug is discovered, you can easily distribute the fix in
+                your apps by merely running <code class="opacity-75 text-lg bg-white rounded p-1">composer update</code>.
             </p>
             <p class="mt-4">
-                There are commercial benefits as well. By open sourcing packages, you as a developer, and your company become much more visible in the community, which can lead to interesting opportunities coming your way.
+                There are commercial benefits as well. By open sourcing packages, you as a developer, and your company
+                become much more visible in the community, which can lead to interesting opportunities coming your way.
             </p>
         </div>
     </blockquote>
@@ -20,7 +23,8 @@
     <div>
         <header class="mb-12 flex items-center">
             <span class="mr-4 flex-none w-24 h-24 rounded-full overflow-hidden shadow-lg border-2 border-white">
-                <img alt="Freek avatar" class="w-24 h-24 object-cover rounded-full overflow-hidden" src="/images/freek.jpg">
+                <img alt="Freek avatar" class="w-24 h-24 object-cover rounded-full overflow-hidden"
+                     src="/images/freek.jpg">
             </span>
             <div>
                 <h2 class="markup-h1">
@@ -32,9 +36,17 @@
 
         <div class="max-w-3xl text-lg markup-links">
             <p>
-                Freek Van der Herten is a developer and partner at <a href="https://spatie.be">Spatie</a>, a Belgium-based company specializing in creating Laravel apps. The company has an open-source first mentality. Freek and his team have released <a href="https://spatie.be/open-source">over 200 packages</a>, that are loved by the community, and which have been downloaded for over 75 million times.
-                <br>
-                Freek also has a passion for spreading his knowledge. In addition to regularly <a href="https://freek.dev/speaking#all-talks">speaking</a> at local user groups and some of the world's best conferences, he also publishes posts on PHP, Laravel, and programming in general at <a href="https://freek.dev/originals">his well-respected blog</a>.
+                Freek Van der Herten is a developer and partner at <a href="https://spatie.be">Spatie</a>, a
+                Belgium-based company specializing in creating Laravel apps. </p>
+            <p class="mt-4">The company has an open-source first mentality. Freek and his team have released <a
+                    href="https://spatie.be/open-source">over 200 packages</a>, that are loved by the community, and
+                which have been downloaded for over 75 million times.
+            </p>
+            <p class="mt-4">
+                Freek also has a passion for spreading his knowledge. In addition to regularly <a
+                    href="https://freek.dev/speaking#all-talks">speaking</a> at local user groups and some of the
+                world's best conferences, he also publishes posts on PHP, Laravel, and programming in general at <a
+                    href="https://freek.dev/originals">his well-respected blog</a>.
             </p>
         </div>
     </div>

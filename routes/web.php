33ee9f66e897bbe1ee6a0df7b522bf88cc826655
freek <freek@spatie.be>
2020-05-22 10:46:06 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,19 @@ use App\Http\Front\Controllers\SubscribeToEmailListController;
 use Illuminate\Support\Facades\Route;
 use Spatie\Honeypot\ProtectAgainstSpam;
 
+Route::demoAccess('/demo');
+
 Route::webhooks('paddle-webhooks');
 Route::post('webhook/github', GitHubWebhookController::class);
 
-Route::get('/', HomeController::class);
-Route::post('subscribe', SubscribeToEmailListController::class)->middleware(ProtectAgainstSpam::class);
+Route::middleware('demoMode')->group(function () {
+    Route::get('/', HomeController::class);
+    Route::post('subscribe', SubscribeToEmailListController::class)->middleware(ProtectAgainstSpam::class);
 
-Route::view('terms-of-use', 'front.legal.terms-of-use')->name('termsOfUse');
-Route::view('privacy', 'front.legal.privacy')->name('privacy');
+    Route::view('terms-of-use', 'front.legal.terms-of-use')->name('termsOfUse');
+    Route::view('privacy', 'front.legal.privacy')->name('privacy');
+});
+
+Route::get('under-construction', function () {
+    return 'under construction...';
+});

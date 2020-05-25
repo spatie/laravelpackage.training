34df4bbc0beae6cf7c76735ec8556311c173a94a
Willem Van Bockstal <willem@spatie.be>
2020-05-25 14:26:52 +0200

    Auth layout

--- resources/views/front/layouts/article.blade.php
+++ resources/views/front/layouts/article.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<main class="z-10 py-16">    
+<main class="z-10 py-16 bg-white shadow-xl">    
     <section class="layout-col">
         <div class="mx-auto max-w-3xl">
             <div class="markup markup-links markup-lists-alt">

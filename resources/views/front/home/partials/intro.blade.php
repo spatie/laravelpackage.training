48b1b10602184199f28188ab3559d2976b182261
Willem Van Bockstal <willem@spatie.be>
2020-05-25 10:08:35 +0200

    Fixes #17

--- resources/views/front/home/partials/intro.blade.php
+++ resources/views/front/home/partials/intro.blade.php
@@ -32,6 +32,7 @@
 
         <template x-if="open">
             <div class="fixed inset-0 p-8 lg:p-16 bg-backdrop z-50 flex items-center justify-center" @keydown.window.escape="open = false">
+                <button class="absolute top-0 right-0 m-6 leading-none text-light-700 text-3xl">&times;</button>
                 <div class="w-full max-w-screen-xl">
                     <div class="bg-white rounded-sm aspect-16x9 shadow-xl">
                         <iframe src="https://player.vimeo.com/video/418813035?autoplay=1" class="absolute inset-0 border-2 border-white rounded-sm" frameborder="0" allow="autoplay; fullscreen" allowfullscreen @click.away="open = false"></iframe>

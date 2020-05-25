ec23a897ed3bc1ae70a1b2d7444d936702eecb9d
freek <freek@spatie.be>
2020-05-25 17:50:16 +0200

    wip

--- config/videos.php
+++ config/videos.php
@@ -21,10 +21,10 @@ return [
                 '418231440', // starting a Laravel package using the skeleton
                 '418391798', // exploring the service provider
                 '418471348', // running tests on GitHub actions
-                '419127047', // using MySQL when running tests
                 '418510042', // installing an unreleased package in a full Laravel app
                 '418813035', // reading config
                 '418904075', // using migrations
+                '419127047', // using MySQL when running tests
                 '419039265', // using routes, controllers and views
                 '418769489', // creating a package together
             ]

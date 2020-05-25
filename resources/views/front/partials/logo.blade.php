08004cdb532d8511fd943a39e46de882005e9a07
Willem Van Bockstal <willem@spatie.be>
2020-05-25 09:44:52 +0200

    Fixes #3

--- resources/views/front/partials/logo.blade.php
+++ resources/views/front/partials/logo.blade.php
@@ -1,9 +1,9 @@
-<a href="/" class="py-2 flex items-center opacity-90 hover:opacity-100">
-    <span class="block w-10 h-10">
+<div class="py-2 flex items-center opacity-90 hover:opacity-100">
+    <a href="/" class="block w-10 h-10">
         <img src="/images/laravel-package-training.svg">
-    </span>
+    </a>
     <span class="ml-4 text-xl font-bold leading-tight">
-        Laravel Package Training
-        <span class="md:absolute top-full left-0 text-xs font-normal opacity-50">a premium video course by <span class="font-semibold tracking-widest">SPATIE</span></span>
+        <a href="/">Laravel Package Training</a>
+        <span class="md:absolute top-full left-0 text-xs font-normal opacity-50">a premium video course by <a href="https://spatie.be" target="_blank" class="markup-link-invers tracking-wider">SPATIE</a></span>
     </span>
-</a>
+</div>

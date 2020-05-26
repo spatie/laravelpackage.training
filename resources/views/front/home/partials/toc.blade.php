49a793d78bb4b07a235e9c7670afc5e53f1677f3
freek <freek@spatie.be>
2020-05-26 08:41:28 +0200

    wip

--- resources/views/front/home/partials/toc.blade.php
+++ resources/views/front/home/partials/toc.blade.php
@@ -21,7 +21,7 @@
                     <li class="pb-3">Using the SPATIE PHP package skeleton to get started building an agnostic package</li>
                     <li class="pb-3">Adding a first class to the package</li>
                     <li class="pb-3">Testing a package using PHPUnit</li>
-                    <li class="pb-3">Automatically fix code styling issue locally</li>
+                    <li class="pb-3">Automatically fix code style issues locally</li>
                     <li class="pb-3">Running the tests on GitHub actions</li>
                     <li class="pb-3">Fixing code style issues using GitHub Actions </li>
                     <li class="pb-3">Supporting multiple PHP versions</li>
@@ -29,7 +29,7 @@
                     <li class="pb-3">Keeping a changelog</li>
                     <li class="pb-3">Registering the package on Packagist</li>
                     <li class="pb-3">Publishing a new release on GitHub</li>
-                    <li class="pb-3">How to take care of community contributions</li>
+                    <li class="pb-3">Taking care of community contributions</li>
             </ul>
         </div>
     </li>
@@ -49,10 +49,10 @@
                     <li class="pb-3">Running the tests of the Laravel package on GitHub Actions</li>
                     <li class="pb-3">Using MySQL in the package tests and on GitHub Actions</li>
                     <li class="pb-3">Testing artisan commands using PHPUnit and Orchestra Testbench</li>
-                    <li class="pb-3">How to test routes, controllers and views</li>
+                    <li class="pb-3">Testing routes, controllers and views</li>
                     <li class="pb-3">Supporting multiple PHP and Laravel versions of your package</li>
-                    <li class="pb-3">How to quickly develop a Laravel package inside a full Laravel application</li>
-                    <li class="pb-3">A whopping 90 minute live coding video where we bring together the knowledge of the entire course to build a real life package from scratch. See Freek code and explain how he thinks.</li>
+                    <li class="pb-3">Developing a Laravel package inside a full Laravel application</li>
+                    <li class="pb-3">A whopping 90 minute live coding video where we bring together the knowledge of the entire course to build a real life package from scratch. Watch Freek code and explain his thought process.</li>
             </ul>
         </div>
     </li>

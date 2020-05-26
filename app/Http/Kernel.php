15ceaf328bc55c0a5206852c0ee33ab2767f7f65
freek <freek@spatie.be>
2020-05-26 22:20:19 +0200

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'canAccessVideos' => \App\Http\Middleware\CanAccessVideos::class,
-        'demoMode' => \Spatie\DemoMode\DemoMode::class,
     ];
 
     protected $middlewarePriority = [
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Front\Controllers\GitHubWebhookController;
 use App\Http\Front\Controllers\HomeController;
 use App\Http\Front\Controllers\PaddleWebhooksController;
 use App\Http\Front\Controllers\SubscribeToEmailListController;
-use App\Http\Front\Controllers\UnderConstructionController;
 use Illuminate\Support\Facades\Route;
 use Spatie\Honeypot\ProtectAgainstSpam;
 
@@ -15,12 +14,8 @@ Route::any('/paddle-webhooks', PaddleWebhooksController::class);
 
 Route::post('webhook/github', GitHubWebhookController::class);
 
-Route::middleware('demoMode')->group(function () {
-    Route::get('/', HomeController::class);
-    Route::post('subscribe', SubscribeToEmailListController::class)->middleware(ProtectAgainstSpam::class);
+Route::get('/', HomeController::class);
+Route::post('subscribe', SubscribeToEmailListController::class)->middleware(ProtectAgainstSpam::class);
 
-    Route::view('terms-of-use', 'front.legal.terms-of-use')->name('termsOfUse');
-    Route::view('privacy', 'front.legal.privacy')->name('privacy');
-});
-
-Route::get('under-construction', UnderConstructionController::class);
+Route::view('terms-of-use', 'front.legal.terms-of-use')->name('termsOfUse');
+Route::view('privacy', 'front.legal.privacy')->name('privacy');

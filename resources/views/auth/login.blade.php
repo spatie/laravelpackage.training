b1f81952b8b1df41081b9d1eb5707482e3a6007d
freek <freek@spatie.be>
2020-08-25 16:31:23 +0200

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,6 @@
 @section('auth')
         <h1>{{ __('Login') }}</h1>
 
-        <div class="alert alert-info text-xl">
-            We are currently performing maintenance. Please check back in a couple of hours.
-            <br/><br/>If you have questions about this, reach out to <a href="mailto:info@spatie.be">info@spatie.be</a>
-        </div>
-
     <div class="alert alert-info text-xl">
         The license and video section of Laravel Package Training has moved to a unified platform.
         From now on, you can log in on <a class="markup-link" href="https://spatie.be/login">https://spatie.be/login</a> to see all of your Spatie products.

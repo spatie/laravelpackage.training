00e6248a2b12686e58c32b386507ef934acb647d
Willem Van Bockstal <willem@spatie.be>
2020-11-19 16:26:06 +0100

    Add coupon counters

--- resources/views/front/home/partials/priceCard.blade.php
+++ resources/views/front/home/partials/priceCard.blade.php
@@ -1,3 +1,7 @@
+@php
+    $coupon = \App\Support\Coupon::forCouponName('default')
+@endphp
+
 <div class="md:grid cols-2 items-stretch block bg-white">
     <div class="p-12">
         <span class="flex flex-col justify-center items-center pb-4 border-b-2 border-blue-100">
@@ -6,25 +10,45 @@
             </h3>
         </span>
 
-        <div class="my-8 text-center">
-            <p class="inline-flex leading-none">
-                <span class="absolute right-full text-blue-300 text-xl mt-1 mr-2" data-id="current-currency"></span>
-                <span class="font-semibold text-blue-900 text-5xl tracking-wide" data-id="current-price">149</span>
-                    {{--
-                    <div style="transform: rotate(-5deg); left: 75%; top: 1rem" class="absolute mr-4 w-16 h-16 flex items-center text-xs tracking-normal leading-tight bg-orange-500 text-white rounded-full shadow-xl">
-                        <span>Ends on <span class="font-semibold">June 4<sup>th</sup></span></span>
+        @if($coupon->active())
+            <div
+                class="flex flex-col items-center mb-6 text-center text-gray-700 text-xs leading-snug">
+                <div class="mt-4 font-bold text-orange-500">{{ $coupon->label() }} ending in</div>
+                <div
+                    class="bg-orange-500 text-white z-10 px-2 py-1"
+                    style="font-variant-numeric:tabular-nums">
+                    <x-countdown :expires="$coupon->expiresAt()">
+                        <span class=""><span
+                                x-text="timer.days">{{ $component->days() }}</span> <span class="">days</span></span>
+                        <span class=""><span
+                                x-text="timer.hours">{{ $component->hours() }}</span> <span class="">hours</span></span>
+                        <span class=""><span
+                                x-text="timer.minutes">{{ $component->minutes() }}</span> <span class="">minutes</span></span>
+                        <span class=""><span
+                                x-text="timer.seconds">{{ $component->seconds() }}</span> <span class="">seconds</span></span>
+                    </x-countdown>
                 </div>
-                --}}
-            {{--
-            <p class="mt-3 text-center uppercase font-medium text-sm text-gray-400">
-                <span class="uppercase tracking-widest">
-                    Introductory price
+            </div>
+        @endif
+
+        <div class="flex justify-center my-8">
+            <div>
+                <sup class="text-gray-500 text-3xl" data-id="current-currency-{{ config('services.paddle.product_id') }}"></sup><span
+                    class="font-bold text-5xl" data-id="current-price-{{ config('services.paddle.product_id') }}">—</span>
+                <span data-id="original-display-{{ config('services.paddle.product_id') }}" class="hidden absolute right-full mr-4 top-0 mt-2">
+                    <sup class="text-gray-500 text-xs" data-id="original-currency-{{ config('services.paddle.product_id') }}"></sup><span
+                        class="text-gray-500 line-through" data-id="original-price-{{ config('services.paddle.product_id') }}">—</span>
                 </span>
-            </p>
-            --}}
+            </div>
         </div>
 
-        <p class="mt-12 text-center">
+         @if ($coupon->active())
+            <div class="mt-4 text-center text-gray-500 text-xs">
+                {{ $coupon->percentage() }}% off with coupon <code class="px-2 py-1 text-gray-700 bg-gray-100">{{ $coupon->code() }}</code>
+            </div>
+        @endif
+
+        <p class="mt-2 text-center">
             <a href="{{ config('settings.buy_url') }}" class="button text-xl">
                 Buy the complete course
             </a>
@@ -60,25 +84,38 @@
     </div>
 
     <script type="text/javascript">
-        function indexOfFirstDigitInString(string) {
-            let firstDigit = string.match(/\d/);
+    function indexOfFirstDigitInString(string) {
+        let firstDigit = string.match(/\d/);
 
-            return string.indexOf(firstDigit);
-        }
+        return string.indexOf(firstDigit);
+    }
 
-        Paddle.Product.Prices(593300, function(prices) {
-            console.log(prices);
+    function displayPaddleProductPrice(productId) {
+        Paddle.Product.Prices(productId, function(prices) {
             let priceString = prices.price.net;
+
+            let factor = {{ $coupon->active() ? (100 - $coupon->percentage())/100 : 1 }};
 
             let indexOFirstDigitInString = indexOfFirstDigitInString(priceString);
 
             let price = priceString.substring(indexOFirstDigitInString);
-            price = price.replace('.00', '');
+            price = price.replace('.00', '').replace(/,/g, '');
 
-            let currencySymbol = priceString.substring(0,indexOFirstDigitInString);
+            let currencySymbol = priceString.substring(0, indexOFirstDigitInString);
             currencySymbol = currencySymbol.replace('US', '');
 
-            document.querySelector('[data-id="current-currency"]').innerHTML = currencySymbol;
-            document.querySelector('[data-id="current-price"]').innerHTML = price;
+            document.querySelector(`[data-id="original-currency-${productId}"]`).innerHTML = currencySymbol;
+            document.querySelector(`[data-id="original-price-${productId}"]`).innerHTML = price;
+
+            document.querySelector(`[data-id="current-currency-${productId}"]`).innerHTML = currencySymbol;
+            document.querySelector(`[data-id="current-price-${productId}"]`).innerHTML = Math.ceil(price * factor);
+            
+            if(factor < 1) {
+                document.querySelector(`[data-id="original-display-${productId}"]`).classList.remove('hidden');
+            }
         });
-    </script>
+    }
+
+    displayPaddleProductPrice({{ config('services.paddle.product_id') }});
+
+</script>

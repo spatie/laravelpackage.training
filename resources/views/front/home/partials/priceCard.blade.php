9b51266e915aba75472c9deecd931e08b77c6557
freek <freek@spatie.be>
2021-11-12 13:32:26 +0100

    wip

--- resources/views/front/home/partials/priceCard.blade.php
+++ resources/views/front/home/partials/priceCard.blade.php
@@ -2,7 +2,7 @@
     <div class="p-12">
         <span class="flex flex-col justify-center items-center pb-4 border-b-2 border-blue-100">
             <h3 class="uppercase font-medium text-sm text-center tracking-widest text-blue-400">
-                4 hours of content
+                Become the next package maestro
             </h3>
         </span>
 
